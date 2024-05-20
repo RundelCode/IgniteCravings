bc6c3ba59f5fc9f222c16826c3c55ba3661ff3fd
morales <juan.morales2004@hotmail.com>
2024-05-20 15:48:55 -0500

    The project was organized by modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+
 
 const routes: Routes = [
-  {path:"", component: HomeComponent}
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'menu', loadChildren: () => import('./modules/menu/menu.module').then(m => m.MenuModule) },
+  { path: 'category', loadChildren: () => import('./modules/category/category.module').then(m => m.CategoryModule) },
+  { path: 'product', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
