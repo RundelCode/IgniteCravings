03da60671826c825d3548567b1cf8017a9c994c9
Javier Gómez <137917002+RundelCode@users.noreply.github.com>
2024-05-21 14:08:57 -0500

    APITest3

--- src/app/modules/shared/navbar/navbar.component.ts
+++ src/app/modules/shared/navbar/navbar.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  allCategories = {
+    categoryId: "1",
+    categoryName: "Name",
+    categoryIcon: "../../../../assets/Icons/homeIcon.png"
+  }
+  categories = [
+    {
+      categoryId: "1",
+      categoryName: "name",
+      categoryIcon: "../../../../assets/Icons/pizza.svg"
+    },
+    {
+      categoryId: "1",
+      categoryName: "name",
+      categoryIcon: "../../../../assets/Icons/wineglass.svg"
+    },
+    {
+      categoryId: "1",
+      categoryName: "name",
+      categoryIcon: "../../../../assets/Icons/beer.svg"
+    },
+    {
+      categoryId: "1",
+      categoryName: "name",
+      categoryIcon: "../../../../assets/Icons/shrimp.svg"
+    },
+    {
+      categoryId: "1",
+      categoryName: "name",
+      categoryIcon: "../../../../assets/Icons/bbq.svg"
+    },   
+     
+  ]
 
 }
