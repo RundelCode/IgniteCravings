fcbe5599583149c8ed61e99d321586af2a3fc9c4
Javier Gómez <137917002+RundelCode@users.noreply.github.com>
2024-05-22 10:57:42 -0500

    Navb

---
+++ src/app/modules/slider/pages/slider-screen/slider-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-screen',
+  templateUrl: './slider-screen.component.html',
+  styleUrls: ['./slider-screen.component.css']
+})
+export class SliderScreenComponent {
+
+}
